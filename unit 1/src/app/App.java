38da64595f008a7fa4a34a7e5288a088bd08e14c
Huntersterk <815025@mystma.org>
2020-04-08 15:41:17 +0000

    add top five movies

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -2,6 +2,11 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1. pacific rim ");
+        System.out.println("2 happy gilmore");
+        System.out.println("3. waterboy");
+        System.out.println("4. avengers ");
+        System.out.println("5.centeral inteligenc ");
+
     }
 }
