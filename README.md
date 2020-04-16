56c90db7277414e01c135049c8deb2d6cba6127f
Huntersterk <815025@mystma.org>
2020-04-16 15:06:18 +0000

    for project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# unit1
+# Hunter Sterk
--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -2,11 +2,17 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. pacific rim ");
-        System.out.println("2 happy gilmore");
-        System.out.println("3. waterboy");
-        System.out.println("4. avengers ");
-        System.out.println("5.centeral inteligenc ");
-
+for (int i = 0; i <= 10; i = i + 1){
+System.out.println("the count is" + i);
+System.out.println("lets keep counting ");
     }
+    for (int i = 22; i <= 28 = i + 2;);
+    System.out.println("the count is" + i);
+    System.out.println("lets keep counting");
+   
+    for (int i =2; i <= 28 = i + 2);
+    System.out.println(16,32,3,-9,-81);
+    for (int i =50; <= 5 =i - 5);
+    System.out.println(1,4,1,4);
+    
 }
