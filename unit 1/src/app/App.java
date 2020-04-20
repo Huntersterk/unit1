ee4e7aad1b0d568fd0c88a5a01f68e90dcbedfe1
Huntersterk <815025@mystma.org>
2020-04-20 15:32:39 +0000

    homework

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -2,17 +2,6 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-for (int i = 0; i <= 10; i = i + 1){
-System.out.println("the count is" + i);
-System.out.println("lets keep counting ");
-    }
-    for (int i = 22; i <= 28 = i + 2;);
-    System.out.println("the count is" + i);
-    System.out.println("lets keep counting");
-   
-    for (int i =2; i <= 28 = i + 2);
-    System.out.println(16,32,3,-9,-81);
-    for (int i =50; <= 5 =i - 5);
-    System.out.println(1,4,1,4);
+
     
 }
