8e17bb85c3a8571427af16e58901b476ecbb9da4
Huntersterk <815025@mystma.org>
2020-04-24 14:28:55 +0000

    quiz 1

--- unit 1/src/app/App.java
+++ unit 1/src/app/App.java
@@ -2,6 +2,17 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
+System.out.println("start first loop");
+        for (int i =10; 0 < i; i + 1){
+           System.out.println(i);
+            if (i ==5; 5 = five);
+            System.out.println("end first loop");
+        }
+    }
+} 
+ {  System.out.println("start 2 loop");
+for(i = 160;i < 99; i + 10);
+if (i == 150: i +1);
+for (i = 200; i < 169; i + 10);
 
-    
 }
